e433fb49c577541e406b6f5bbf8746ff25335081
ZeroDaybr <mahfujurahman2020@gmail.com>
2023-09-03 02:08:56 +0600

    Login page and register page modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,100 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+ <!--start content-->
+ <main class="authentication-content">
+    <div class="container">
+      <div class="mt-4">
+        <div class="card rounded-0 overflow-hidden shadow-none border mb-5 mb-lg-0">
+          <div class="row g-0">
+            <div class="col-12 order-1 col-xl-8 d-flex align-items-center justify-content-center border-end">
+              <img src="admin/assets/images/error/auth-img-7.png" class="img-fluid" alt="">
+            </div>
+            <div class="col-12 col-xl-4 order-xl-2">
+              <div class="card-body p-4 p-sm-5">
+                <h5 class="card-title">Sign In</h5>
+                <p class="card-text mb-4">See your growth and get consulting support!</p>
+                 <form class="form-body" method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="row g-3">
+                      <div class="col-12">
+                        <label for="email" class="form-label">{{ __('Email Address') }}</label>
+
+                        <div class="ms-auto position-relative">
+                          <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="bi bi-envelope-fill"></i></div>
+
+                          <input id="email" type="email" placeholder="Email" class="form-control radius-30 ps-5 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                      </div>
+                      <div class="col-12">
+
+                        <label for="password" class="form-label">{{ __('Password') }}</label>
+
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                        <div class="ms-auto position-relative">
+                          <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="bi bi-lock-fill"></i></div>
+
+                          <input type="password" class="form-control radius-30 ps-5" type="password" id="password" placeholder="Password" @error('password') is-invalid @enderror name="password" required autocomplete="current-password">
+
+                          @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="form-check form-switch">
+                          <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                          <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                        </div>
+                      </div>
+                      <div class="col-6 text-end">	<a href="authentication-forgot-password.html">Forgot Password ?</a>
+                      </div>
+                      <div class="col-12">
+                        <div class="d-grid">
+                          <button type="submit" class="btn btn-primary radius-30">{{ __('Login') }}</button>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="login-separater text-center"> <span>OR SIGN IN WITH EMAIL</span>
+                          <hr>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="d-flex align-items-center gap-3 justify-content-center">
+                          <button type="button" class="btn btn-white text-danger"><i class="bi bi-google me-0"></i></button>
+                          <button type="button" class="btn btn-white text-primary"><i class="bi bi-linkedin me-0"></i></button>
+                          <button type="button" class="btn btn-white text-info"><i class="bi bi-facebook me-0"></i></button>
+                        </div>
+                      </div>
+                      <div class="col-12 text-center">
+                        <p class="mb-0">Don't have an account yet? <a href="authentication-signup-with-header-footer.html">Sign up here</a></p>
+                      </div>
+                    </div>
+                </form>
+             </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </main>
+
+   <!--end page main-->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,155 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{-- Registration and login only temporary--}}
+
+<!DOCTYPE html>
+<html lang="en" class="minimal-theme">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="admin/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="admin/assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+    <link href="admin/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="admin/assets/css/bootstrap-extended.css" rel="stylesheet" />
+    <link href="admin/assets/css/style.css" rel="stylesheet" />
+    <link href="admin/assets/css/icons.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- loader-->
+    <link href="admin/assets/css/pace.min.css" rel="stylesheet" />
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!--Theme Styles-->
+    <link href="admin/assets/css/dark-theme.css" rel="stylesheet" />
+    <link href="admin/assets/css/light-theme.css" rel="stylesheet" />
+    <link href="admin/assets/css/semi-dark.css" rel="stylesheet" />
+    <link href="admin/assets/css/header-colors.css" rel="stylesheet" />
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>Skodash - Bootstrap 5 Admin Template</title>
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+<body class="bg-surface">
+            <div class="wrapper">
 
-                    </ul>
+                <header>
+                   <nav class="navbar navbar-expand-lg navbar-light bg-white rounded-0 border-bottom">
+                     <div class="container">
+                       <a class="navbar-brand" href="#"><img src="admin/assets/images/brand-logo-2.png" width="140" alt=""/></a>
+                       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                         <span class="navbar-toggler-icon"></span>
+                       </button>
+                       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                         <ul class="navbar-nav mb-2 mb-lg-0 align-items-center">
+                           <li class="nav-item">
+                             <a class="nav-link" href="#">Home</a>
+                           </li>
+                           <li class="nav-item">
+                             <a class="nav-link" href="javascript:;">About</a>
+                           </li>
+                           <li class="nav-item dropdown">
+                             <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
+                                Services <i class="bi bi-chevron-down align-middle ms-2"></i>
+                             </a>
+                             <ul class="dropdown-menu">
+                               <li><a class="dropdown-item" href="#">Action</a></li>
+                               <li><a class="dropdown-item" href="#">Another action</a></li>
+                               <li><hr class="dropdown-divider"></li>
+                               <li><a class="dropdown-item" href="#">Something else here</a></li>
+                             </ul>
+                           </li>
+                           <li class="nav-item">
+                             <a class="nav-link" href="javascript:;">Contact Us</a>
+                           </li>
+                         </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                           <li class="nav-item dropdown">
+                             <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
+                               English
+                             </a>
+                             <ul class="dropdown-menu">
+                               <li><a class="dropdown-item" href="#">Action</a></li>
+                               <li><a class="dropdown-item" href="#">Another action</a></li>
+                               <li><hr class="dropdown-divider"></li>
+                               <li><a class="dropdown-item" href="#">Something else here</a></li>
+                             </ul>
+                           </li>
+                           <li class="nav-item">
+                             <a class="nav-link" href="javascript:;">Support</a>
+                           </li>
+                         </ul>
+
+
+                         @guest
+                         <div class="d-flex ms-3 gap-3">
+
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             @endif
-
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            @else
+                            <div class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                            </div>
+                         </div>
+                         @endguest
+                       </div>
+                     </div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
+
+
+
+                   </nav>
+                </header>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+
+        <!--end page main-->
+
+        <footer class="bg-white border-top p-3 text-center fixed-bottom">
+            <p class="mb-0">Copyright © 2021. All right reserved.</p>
+        </footer>
+
     </div>
+    <!--end wrapper-->
+
+<!-- Bootstrap bundle JS -->
+<script src="admin/assets/js/bootstrap.bundle.min.js"></script>
+<!--plugins-->
+<script src="admin/assets/js/jquery.min.js"></script>
+<script src="admin/assets/plugins/simplebar/js/simplebar.min.js"></script>
+<script src="admin/assets/plugins/metismenu/js/metisMenu.min.js"></script>
+<script src="admin/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+<script src="admin/assets/js/pace.min.js"></script>
+<!--app-->
+<script src="admin/assets/js/app.js"></script>
+
 </body>
+
 </html>
