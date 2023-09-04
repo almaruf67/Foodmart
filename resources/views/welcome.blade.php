ffc1c7d2734b2ab4176a51a6a580c75b0623bf1d
Nico <afifatun15-13972@diu.edu.bd>
2023-09-04 17:43:26 +0600

    popular update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,8 +103,7 @@
             </header>
             <button class="left" onclick="leftScroll()"><i class="fas fa-chevron-left"></i></button>
             <div class="ppDishes col-12">
-                {{-- <button id="slideLeft" type="button">Slide left</button>
-                  <button id="slideRight" type="button">Slide right</button> --}}
+                
                 <!-- PopularCol1 -->
                 <div class="fCol px-3 mb-6">
                     <div class="shadow p-3 mb-5 bg-body rounded">
@@ -661,6 +660,7 @@
                     </div>
                 </div>
             </div>
+            
 
             </div>
         </section>
@@ -1055,16 +1055,7 @@
 @endsection
 @section('script')
     <script>
-        // const buttonRight = document.getElementById('slideRight');
-        // const buttonLeft = document.getElementById('slideLeft');
-
-        // buttonRight.onclick = function () {
-        //   document.getElementById('ppDishes').scrollLeft += 20;
-        // };
-        // buttonLeft.onclick = function () {
-        //   document.getElementById('ppDishes').scrollLeft -= 20;
-        // };
-
+       
         function leftScroll() {
             const left = document.querySelector(".ppDishes");
             left.scrollBy(-200, 0);
@@ -1074,24 +1065,6 @@
             const right = document.querySelector(".ppDishes");
             right.scrollBy(200, 0);
         }
-
-        //       function sDesert() {
-        //     var x = document.getElementById('dessert');
-        //     if (x.style.display == 'none') {
-        //         x.style.display = 'block';
-        //     } else {
-        //         x.style.display = 'none';
-        //     }
-        // }
-        // function myFunc(drinks) {
-        //     var x = document.getElementById('pizza');
-
-        //     if (x.style.display == 'none') {
-        //         x.style.display = 'block';
-        //     } else {
-        //         x.style.display = 'none';
-        //     }
-        // }
 
 		let currentDiv = null;
         function showDiv(divId) {
