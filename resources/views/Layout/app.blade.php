43af33c7911551dd8a77f57cbf6547913fbbe92a
Maruf <almaruf67@gmail.com>
2023-09-11 02:12:42 +0600

    updated user login condition on navbar with drop down menu

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -15,7 +15,7 @@
         rel="stylesheet">
     <!-- include the site bootstrap stylesheet -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-	
+
     <!-- include the site fontawesome stylesheet -->
     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
     <!-- include the site stylesheet -->
@@ -30,87 +30,120 @@
 </head>
 
 <body>
-  <!-- pageWrapper -->
-	<div id="pageWrapper">
-		<!-- header -->
-		<header id="header" class="position-fixed bg-white">
-			
-			<!-- headerHolder -->
-			<div class="headerHolder container pt-lg-2 pb-lg-2 py-2">
-				<div class="row">
-					<div class="col-6 col-sm-2">
-						<!-- mainLogo -->
-						<div class="logo">
-							<a href="home.html"><img src="{{ asset('assets/images/logo1.png') }}" alt="Botanical" class="img-fluid"></a>
-						</div>
-					</div>
-					<div class="col-6 col-sm-7 col-lg-8 static-block">
-						<!-- mainHolder -->
-						<div class="mainHolder pt-lg-5 pt-3 justify-content-center">
-							<!-- pageNav2 -->
-							<nav class="navbar navbar-expand-lg navbar-light p-0 pageNav2 position-static">
-								<button type="button" class="navbar-toggle collapsed position-relative" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-								<div class="collapse navbar-collapse" id="navbarNav">
-									<ul class="navbar-nav mx-auto text-uppercase d-inline-block">
-										<li class="nav-item">
-                                            <a class="d-block" href="{{route('home')}}">Home</a>
+    <!-- pageWrapper -->
+    <div id="pageWrapper">
+        <!-- header -->
+        <header id="header" class="position-fixed bg-white">
 
-										</li>
-										<li class="nav-item dropdown">
-											<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Store</a>
-											<ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
-												<li class="d-block mx-0"><a href="shop.html">Shop Left Sidebar</a></li>
-												<li class="d-block mx-0"><a href="shop-detail.html">Single Product</a></li>
-											</ul>
-										</li>
-										<li class="nav-item">
-											<a class="d-block" href="about-us.html">About</a>
-										</li>
-										<li class="nav-item dropdown">
-											<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
-											<ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
-												<li class="d-block mx-0"><a href="blog.html">Blog Left Sidebar</a></li>
-												<li class="d-block mx-0"><a href="blog-detail.html">Blog Detail</a></li>
-											</ul>
-										</li>
-										<li class="nav-item dropdown">
-											<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
-											<ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
-												<li class="d-block mx-0"><a href="cart-page.html">Cart Page</a></li>
-											</ul>
-										</li>
-										<li class="nav-item">
-											<a class="d-block" href="contact-us.html">contact</a>
-										</li>
-										
-									</ul>
-								</div>
-							</nav>
-						</div>
-					</div>
-					<div class="col-sm-1 col-lg-2">
-						<!-- wishListII -->
-						<ul class="nav nav-tabs wishListII justify-content-end align-items-center pt-2 border-bottom-0">
-							<li class="nav-item ml-0"><a class="nav-link icon-search" href="javascript:void(0);"></a></li>
-							{{-- <li class="nav-item"><a class="nav-link position-relative icon-cart" href="javascript:void(0);"><span class="num rounded d-block">2</span></a></li>
-							<li class="nav-item"><a class="nav-link icon-profile" href="javascript:void(0);"></a></li> --}}
-							<li class="nav-item"><a class="btn btnTheme fwEbold text-white sm-round py-2 px-3" href="">Login</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</header>
+            <!-- headerHolder -->
+            <div class="headerHolder container pt-lg-2 pb-lg-2 py-2">
+                <div class="row">
+                    <div class="col-6 col-sm-2">
+                        <!-- mainLogo -->
+                        <div class="logo">
+                            <a href="home.html"><img src="{{ asset('assets/images/logo1.png') }}" alt="Botanical"
+                                    class="img-fluid"></a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-7 col-lg-8 static-block">
+                        <!-- mainHolder -->
+                        <div class="mainHolder pt-lg-5 pt-3 justify-content-center">
+                            <!-- pageNav2 -->
+                            <nav class="navbar navbar-expand-lg navbar-light p-0 pageNav2 position-static">
+                                <button type="button" class="navbar-toggle collapsed position-relative"
+                                    data-toggle="collapse" data-target="#navbarNav" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav mx-auto text-uppercase d-inline-block">
+                                        <li class="nav-item">
+                                            <a class="d-block" href="{{ route('home') }}">Home</a>
+
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="dropdown-toggle d-block" role="button" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Store</a>
+                                            <ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
+                                                <li class="d-block mx-0"><a href="shop.html">Shop Left Sidebar</a></li>
+                                                <li class="d-block mx-0"><a href="shop-detail.html">Single Product</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="d-block" href="about-us.html">About</a>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="dropdown-toggle d-block" role="button" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Blog</a>
+                                            <ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
+                                                <li class="d-block mx-0"><a href="blog.html">Blog Left Sidebar</a></li>
+                                                <li class="d-block mx-0"><a href="blog-detail.html">Blog Detail</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="dropdown-toggle d-block" role="button" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Pages</a>
+                                            <ul class="list-unstyled text-capitalize dropdown-menu mt-0 py-0">
+                                                <li class="d-block mx-0"><a href="cart-page.html">Cart Page</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="d-block" href="contact-us.html">contact</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-sm-1 col-lg-2 dropdown">
+                        <!-- wishListII -->
+                        <ul class="nav nav-tabs wishListII justify-content-end align-items-center pt-2 border-bottom-0">
+                            <li class="nav-item ml-0"><a class="nav-link icon-search" href="javascript:void(0);"></a>
+                            </li>
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item"><a class="btn btnTheme fwEbold text-white sm-round py-2 px-3"
+                                            href="{{ route('login') }}">Login</a></li>
+                                @endif
+                            @else
+                                <li class="nav-item"><a class="nav-link position-relative icon-cart"
+                                        href="javascript:void(0);"><span class="num rounded d-block">2</span></a></li>
+                                <li class="nav-item"><a class="nav-link icon-profile" href="javascript:void(0);"></a></li>
+                                <li class="nav-item"><a id="navbarDropdown" class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{ Auth::user()->name }}
+                                    </a>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#">
+							    Orders
+							</a>
+							<a class="dropdown-item" href="{{ route('logout') }}"
+							    onclick="event.preventDefault();
+						   document.getElementById('logout-form').submit();">
+							    LogOut
+							</a>
+	
+							<form id="logout-form" action="{{ route('logout') }}" method="POST"
+							    class="d-none">
+							    @csrf
+							</form>
+						  </div>
+					</li>	  
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
         <!-- main -->
         @yield('content')
         <!-- footer -->
         <footer id="footer" class="container-fluid overflow-hidden px-lg-20">
             <div class="copyRightHolder text-center pt-lg-5 pb-lg-4 py-3">
-                <p class="mb-0">Coppyright 2019 by <a href="javascript:void(0);">Botanical Store</a> - All right
+                <p class="mb-0">Coppyright 2023 by <a href="javascript:void(0);">Yumzy Food</a> - All right
                     reserved</p>
             </div>
         </footer>
@@ -123,7 +156,7 @@
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     <!-- include custom JavaScript -->
     <script src="{{ asset('assets/js/jqueryCustome.js') }}"></script>
-@yield('script')
+    @yield('script')
 
 
 </body>
