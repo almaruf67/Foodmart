28fe298ac03814b78a1c16fe0e097485c4115690
Maruf <almaruf67@gmail.com>
2023-09-18 18:26:26 +0600

    fixed phone update

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use Auth;
+use Exception;
 use App\Models\User;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Validator;
 
 class ProviderController extends Controller
@@ -52,31 +53,7 @@ class ProviderController extends Controller
             dd($e->getMessage());
         }
     }
-    // public function callback($provider)
-    // {
-    //     $password = Str::password(10);
-    //     // dd($provider);
-    //     $provideruser = Socialite::driver($provider)->user();
-    //     dd($provideruser);
 
-
-
-    // $user = User::updateOrCreate([
-    //     'provider_id'=>$provideruser->id,
-    //     'provider' => $provider,
-    // ], [
-    //     'name' => $provideruser->name,
-    //     'email' => $provideruser->email,
-    //     'password' => $password,
-    //     'type'=> 'user',
-    //     'image' => $provideruser->getAvatar(),
-
-    // ]);
-
-    //     Auth::login($user);
-
-    //     return redirect('/home');
-    // }
 
 
 }
